4282d72dc93b32473d3271cd27fb1ed6c41d7afd
EstherChenGithub <maru@77yen.com>
2022-11-10 09:43:52 +0800

    updated

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/SmileMaru/vue-project/"
+  publicPath: "/vue-project/dist/"
 })
