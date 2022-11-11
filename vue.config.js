a8426e3a567fcce99b37c84c48b3694fe9afa8f5
EstherChenGithub <maru@77yen.com>
2022-11-11 15:40:14 +0800

    renamed path built

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/vue-project/dist/"
+  publicPath: "/The-4th-F2E/dist/"
 })
